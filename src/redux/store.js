963fbe68c74dda254e935b383c73acdba11cf359
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2026-01-17 11:56:36 +0000

    add localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,20 @@
 import { createStore } from "redux";
 
+const loadContacts = () => {
+  const saved = localStorage.getItem("contacts");
+  return saved
+    ? JSON.parse(saved)
+    : [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ];
+};
+
 const initialState = {
   contacts: {
-    items: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    items: loadContacts(),
   },
   filters: {
     name: "",
@@ -40,3 +47,7 @@ const reducer = (state = initialState, action) => {
 };
 
 export const store = createStore(reducer);
+store.subscribe(() => {
+  const { contacts } = store.getState();
+  localStorage.setItem("contacts", JSON.stringify(contacts.items));
+});
